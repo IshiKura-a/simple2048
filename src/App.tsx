8100add7dbbc9f62245a4cfe7dddededca77393b
IshiKura-a <1563415484@qq.com>
2021-02-22 16:14:15 +0800

    尝试修改状态更新的bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
 import Block from "./Block";
+import Timer from "./Timer";
 
 function App() {
     type Data = number[]
@@ -8,109 +9,107 @@ function App() {
     type PosFunc = (i: number, j: number) => number
     type Validity = (prevIndex: number, curIndex: number) => boolean
 
-    const [time, incTimer] = useState(0)
+    interface MergeFunc {
+        prevPos: PosFunc,
+        curPos: PosFunc,
+        isValid: Validity
+    }
+
     const [data, updateData] = useState(
         [2, 2, 0, 0,
             0, 0, 0, 0,
             0, 0, 0, 0,
             0, 0, 0, 0] as Data)
 
+
     function getScore(): number {
         return data.reduce((accumulator, currentValue) => accumulator + currentValue)
     }
 
 
-
     useEffect(() => {
-        window.addEventListener("keypress", (e: any) => {
-            console.log(e.key)
-            let newState: Data = data.concat()
-
-            function randomNewNumber(): Data {
-                let zeros = newState.map((v, idx) => {
-                    if (v === 0) {
-                        return idx
-                    } else return 0
-                }).filter(v => v !== 0)
-                newState[zeros.reduce((accumulator, currentValue) => {
-                    if (Math.random() >= 1 / zeros.length) {
-                        return accumulator
-                    } else return currentValue
-                }, zeros[0])] = 2
-                console.log("newState", newState)
-                return newState
+        const mergeFuncList: { [key: string]: MergeFunc } = {
+            "w": {
+                prevPos: (i, j) => 4 * j + i - 4,
+                curPos: (i, j) => 4 * j + i,
+                isValid: (prevIndex, curIndex) => prevIndex >= 0
+            },
+            "a": {
+                prevPos: (i, j) => 4 * i + j - 1,
+                curPos: (i, j) => 4 * i + j,
+                isValid: (prevIndex, curIndex) => Math.floor(prevIndex / 4) === Math.floor(curIndex / 4)
+            },
+            "s": {
+                prevPos: (i, j) => 16 - 4 * j + i,
+                curPos: (i, j) => 12 - 4 * j + i,
+                isValid: (prevIndex, curIndex) => curIndex < 16
+            },
+            "d": {
+                prevPos: (i, j) => 4 * i + 4 - j,
+                curPos: (i, j) => 4 * i + 3 - j,
+                isValid: (prevIndex, curIndex) => Math.floor(prevIndex / 4) === Math.floor(curIndex / 4)
             }
+        }
 
-            function merge(prevPos: PosFunc, curPos: PosFunc, isValid: Validity) {
-                for (let i = 0; i < 4; i++) {
-                    let flag = true
-                    while (flag) {
-                        flag = false
-                        for (let j = 0; j < 4; j++) {
-                            const prevIndex = prevPos(i, j)
-                            const curIndex = curPos(i, j)
-                            if (!isValid(prevIndex, curIndex) || newState[curIndex] === 0)
-                                continue
-                            if (newState[prevIndex] === 0) {
-                                flag = true
-                                newState[prevIndex] = newState[curIndex]
-                                newState[curIndex] = 0
-                            } else if (newState[prevIndex] === newState[curIndex]) {
-                                flag = true
-                                newState[prevIndex] *= 2
-                                newState[curIndex] = 0
-                            }
+        function randomNewNumber(state: Data): Data {
+            let zeros = state.map((v, idx) => v === 0 ? idx : 0).filter(v => v !== 0)
+            state[zeros.reduce((accumulator, currentValue) =>
+                Math.random() >= 1 / zeros.length ? accumulator : currentValue, zeros[0])] = 2
+            return state
+        }
+
+        function merge(state: Data, prevPos: PosFunc, curPos: PosFunc, isValid: Validity): Data {
+            for (let i = 0; i < 4; i++) {
+                let flag = true
+                while (flag) {
+                    flag = false
+                    for (let j = 0; j < 4; j++) {
+                        const prevIndex = prevPos(i, j)
+                        const curIndex = curPos(i, j)
+                        if (!isValid(prevIndex, curIndex) || state[curIndex] === 0)
+                            continue
+                        if (state[prevIndex] === 0) {
+                            flag = true
+                            state[prevIndex] = state[curIndex]
+                            state[curIndex] = 0
+                        } else if (state[prevIndex] === state[curIndex]) {
+                            flag = true
+                            state[prevIndex] *= 2
+                            state[curIndex] = 0
                         }
                     }
                 }
             }
+            return state
+        }
 
-            if (e.key === "w") {
-                merge((i, j) => 4 * j + i - 4,
-                    (i, j) => 4 * j + i,
-                    (prevIndex, curIndex) => prevIndex >= 0)
-                updateData(randomNewNumber())
-            } else if (e.key === "a") {
-                merge((i, j) => 4 * i + j - 1,
-                    (i, j) => 4 * i + j,
-                    (prevIndex, curIndex) =>
-                        Math.floor(prevIndex / 4) === Math.floor(curIndex / 4))
-                updateData(randomNewNumber())
-            } else if (e.key === "s") {
-                merge((i, j) => 16 - 4 * j + i,
-                    (i, j) => 12 - 4 * j + i,
-                    (prevIndex, curIndex) => curIndex < 16)
-                updateData(randomNewNumber())
-            } else if (e.key === "d") {
-                merge((i, j) => 4 * i + 4 - j,
-                    (i, j) => 4 * i + 3 - j,
-                    (prevIndex, curIndex) =>
-                        Math.floor(prevIndex / 4) === Math.floor(curIndex / 4))
-                updateData(randomNewNumber())
+        window.addEventListener("keypress", (e: any) => {
+            if (["w", "a", "s", "d"].includes(e.key)) {
+                console.log(e.key)
+                const func = mergeFuncList[e.key]
+                const state = randomNewNumber(merge(data.concat(), func.prevPos, func.curPos, func.isValid).concat())
+                updateData(state)
             } else if (e.key === "u") {
                 console.log(data)
             }
         })
-
-        let timer = setInterval(() => {
-            incTimer((time: number) => time + 1)
-        }, 1000)
-        return () => {
-            clearInterval(timer)
-        }
     }, [])
 
-    let tmp = data.map((v, index) => <td key={index}><Block value={v}/></td>)
+    function rawElements(beginIndex: number, endIndex: number) {
+        return data.map((v, index) => <td key={index}><Block value={v}/></td>)
+            .filter((v, index) => index >= beginIndex && index < endIndex)
+    }
+
     return (
-        <div className="App" onKeyDown={e => alert(e.key)}>
+        <div className="App">
             <h2>Simple 2048 Project</h2>
-            <div className="Timer">Time:{time}s</div>
+            <Timer/>
             <div className="Score">Score:{getScore()}</div>
             <table className="Game">
-                <tr>{tmp.filter((v, index) => index >= 0 && index < 4)}</tr>
-                <tr>{tmp.filter((v, index) => index >= 4 && index < 8)}</tr>
-                <tr>{tmp.filter((v, index) => index >= 8 && index < 12)}</tr>
-                <tr>{tmp.filter((v, index) => index >= 12 && index < 16)}</tr>
+                <tr>{rawElements(0, 4)}</tr>
+                <tr>{rawElements(4, 8)}</tr>
+                <tr>{rawElements(8, 12)}</tr>
+                <tr>{rawElements(12, 16)}</tr>
             </table>
 
         </div>
